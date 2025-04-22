8c6d775d2ef314cc37dbd7548e115ed0b6a3da79
Edvard Andreassen <mitori0211@gmail.com>
2025-04-22 15:05:47 +0200

    Updated VisualObject class

---
+++ VisualObject.cpp
@@ -0,0 +1,21 @@
+#include "VisualObject.h"
+
+VisualObject::VisualObject()
+{
+    mMatrix.setToIdentity();
+}
+
+void VisualObject::move(float x, float y, float z)
+{
+    mMatrix.translate(x, y, z);
+}
+
+void VisualObject::scale(float s)
+{
+    mMatrix.scale(s);
+}
+
+void VisualObject::rotate(float t, float x, float y, float z)
+{
+    mMatrix.rotate(t, x, y, z);
+}
